d252930549004515389171a0dfed36f849e2503a
arup10leo <83360443+arup10leo@users.noreply.github.com>
2023-06-15 10:06:13 +0530

    Update app2.py

--- app2.py
+++ app2.py
@@ -45,18 +45,6 @@ if st.button('Predict Price'):
  cluster=int(pipe.predict(query))
  st.title(cluster)
  
-#  cluster_df1=df[df['Cluster']==cluster]
-# #  rcParams["figure.set_size_inches"] = (10, 6)
-#  plt.rcParams['figure.figsize'] = (10, 6)
-#  for c in cluster_df1.drop(['Cluster'],axis=1):
-#   fig, ax = plt.subplots()
-#   grid= sns.FacetGrid(cluster_df1, col='Cluster')
-#   grid= grid.map(plt.hist, c)
-#   plt.show()
-#   st.pyplot(fig)
- for c in df.drop(['Cluster'],axis=1):
-    grid= sns.FacetGrid(df, col='Cluster')
-    fig= grid.map(plt.hist, c)
-    st.pyplot(fig)
+
 
  
